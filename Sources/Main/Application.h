f04a72e4542b128daa6e00555e0d40bef691f99e
FromtonRouge <fromtonrouge@gmail.com>
2020-08-05 22:42:18 +0200

    Lessons Editor cancelled

--- Sources/Main/Application.h
+++ Sources/Main/Application.h
@@ -33,8 +33,6 @@ class KeyboardModel;
 class UndoableKeyboardModel;
 class TheoryModel;
 class UndoableTheoryModel;
-class LessonsModel;
-class UndoableLessonsModel;
 class Application : public QApplication
 {
     Q_OBJECT
@@ -51,8 +49,6 @@ public:
     KeyboardModel* getKeyboardModel() const {return _pKeyboardModel;}
     UndoableKeyboardModel* getUndoableKeyboardModel() const {return _pUndoableKeyboardModel;}
     KeyboardGraphicsScene* getKeyboardGraphicsScene() const {return _pKeyboardGraphicsScene;}
-    LessonsModel* getLessonsModel() const {return _pLessonsModel;}
-    UndoableLessonsModel* getUndoableLessonsModel() const {return _pUndoableLessonsModel;}
 
 private:
     void toLogs(const QString& sText, int iWarningLevel = 0);
@@ -68,6 +64,4 @@ private:
     KeyboardModel* _pKeyboardModel;
     UndoableKeyboardModel* _pUndoableKeyboardModel;
     KeyboardGraphicsScene* _pKeyboardGraphicsScene; ///< Only 1 scene and potentialy N views on this scene.
-    LessonsModel* _pLessonsModel;
-    UndoableLessonsModel* _pUndoableLessonsModel;
 };
